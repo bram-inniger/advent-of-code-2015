c6bdaeec7c1333cf171e5a942f3ba6c349a5687e
Bram Inniger <bram@inniger.be>
2024-01-05 23:19:59 +0200

    Project set-up

---
+++ src/solutions/day_01.rs
@@ -0,0 +1,41 @@
+// use std::str::FromStr;
+
+// use regex::Regex;
+
+pub fn solve_1(X: &[&str]) -> u32 {
+    42
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+
+    use super::*;
+
+    #[test]
+    fn day_01_part_01_sample() {
+        let sample = vec![
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        ];
+
+        assert_eq!(42, solve_1(&sample));
+    }
+
+    #[test]
+    fn day_01_part_01_solution() {
+        let input = include_str!("../../inputs/day_01.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(0, solve_1(&input));
+    }
+}
