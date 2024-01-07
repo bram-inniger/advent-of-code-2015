8ae64c51709034c782875b34d4cf4e1ff8f8aab6
Bram Inniger <bram@inniger.be>
2024-01-08 01:11:08 +0200

    Solved D19P02

--- src/solutions/day_19.rs
+++ src/solutions/day_19.rs
@@ -5,6 +5,19 @@ pub fn solve_1(machine: &str) -> usize {
     Machine::new(machine).generate_molecules().len()
 }
 
+// Answer based on https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4h7ji/
+// Answer = #NumSymbols - #Rn - #Ar - 2 * #Y - 1
+pub fn solve_2(machine: &str) -> usize {
+    let molecule = Machine::new(machine).molecule;
+
+    let nr_symbols = molecule.chars().filter(|c| c.is_uppercase()).count();
+    let nr_rn = molecule.match_indices("Rn").count();
+    let nr_ar = molecule.match_indices("Ar").count();
+    let nr_y = molecule.match_indices('Y').count();
+
+    nr_symbols - nr_rn - nr_ar - 2 * nr_y - 1
+}
+
 #[derive(Debug)]
 struct Machine<'a> {
     transformations: Vec<(&'a str, &'a str)>,
@@ -75,5 +88,35 @@ mod tests {
         let input = include_str!("../../inputs/day_19.txt").trim();
 
         assert_eq!(509, solve_1(input));
+    }
+
+    #[test]
+    fn day_19_part_02_sample() {
+        let sample = "e => H\n\
+            e => O\n\
+            H => HO\n\
+            H => OH\n\
+            O => HH\n\
+            \n\
+            HOH";
+
+        assert_eq!(3 - 1, solve_2(sample));
+
+        let sample = "e => H\n\
+            e => O\n\
+            H => HO\n\
+            H => OH\n\
+            O => HH\n\
+            \n\
+            HOHOHO";
+
+        assert_eq!(6 - 1, solve_2(sample));
+    }
+
+    #[test]
+    fn day_19_part_02_solution() {
+        let input = include_str!("../../inputs/day_19.txt").trim();
+
+        assert_eq!(195, solve_2(input));
     }
 }
