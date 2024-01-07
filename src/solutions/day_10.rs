f6fe69eda5dac9d8148fe98d14b0eab7682d49a3
Bram Inniger <bram@inniger.be>
2024-01-07 02:58:11 +0200

    Solved D10P02

--- src/solutions/day_10.rs
+++ src/solutions/day_10.rs
@@ -1,10 +1,10 @@
 use crate::util::BASE_10;
 use itertools::Itertools;
 
-pub fn solve_1(start: &str, nr_steps: u8) -> usize {
+pub fn solve(start: &str, nr_steps: u8) -> usize {
     let mut number = start
         .chars()
-        .map(|c| c.to_digit(BASE_10).unwrap())
+        .map(|c| c.to_digit(BASE_10).unwrap() as u8)
         .collect_vec();
 
     (0..nr_steps).for_each(|_| number = expand(&number));
@@ -12,7 +12,7 @@ pub fn solve_1(start: &str, nr_steps: u8) -> usize {
     number.len()
 }
 
-fn expand(number: &[u32]) -> Vec<u32> {
+fn expand(number: &[u8]) -> Vec<u8> {
     let mut expanded = vec![];
 
     let mut digit = number[0];
@@ -44,13 +44,25 @@ mod tests {
     fn day_10_part_01_sample() {
         let sample = "1";
 
-        assert_eq!(6, solve_1(sample, 5));
+        assert_eq!(6, solve(sample, 5));
     }
 
     #[test]
     fn day_10_part_01_solution() {
         let input = include_str!("../../inputs/day_10.txt").trim();
 
-        assert_eq!(252_594, solve_1(input, 40));
+        assert_eq!(252_594, solve(input, 40));
+    }
+
+    #[test]
+    fn day_10_part_02_sample() {
+        // No sample inputs for part 2
+    }
+
+    #[test]
+    fn day_10_part_02_solution() {
+        let input = include_str!("../../inputs/day_10.txt").trim();
+
+        assert_eq!(3_579_328, solve(input, 50));
     }
 }
